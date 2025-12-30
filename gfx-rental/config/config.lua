2840d96032847206353576e91e18fc3c603591cd
Grafix <139239309+GFX-Code@users.noreply.github.com>
2025-12-30 11:13:39 +0000

    Add files via upload

--- gfx-rental/config/config.lua
+++ gfx-rental/config/config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.RentalItem = "rental_papers"
 
 -- Vehicle Keys System: 'qbx' = qbx_vehiclekeys (item or default version, both work), 'renewed' = Renewed-Vehiclekeys
--- i have a version of qbx_vehiclekeys that works with item-based keys, but the default version also works fine
+-- i have a version of qbx_vehiclekeys that works with item-based keys which can be found here https://github.com/CodexisPhantom/qbx_vehiclekeys/tree/main, but the default version also works fine
 Config.VehicleKeys = 'qbx'
 
 Config.PedModel = "u_m_y_smugmech_01"
